7a5d62042a5abb192c21994494653deb9fcc1f7c
timicienio <timmy960072@gmail.com>
2021-11-04 00:58:43 +0800

    avatar done

---
+++ src/partials/Avatar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import InitialAvatar from 'react-initials-avatar';
+
+import 'react-initials-avatar/lib/ReactInitialsAvatar.css';
+import '../css/additional-styles/avatar.css';
+
+export default function Avatar({ name, size = 'md' }) {
+  return <InitialAvatar name={name} className={`initials-avatar ${size}`} />;
+}
