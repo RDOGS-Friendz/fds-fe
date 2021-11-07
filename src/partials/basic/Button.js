ac0569ac743d9dd84cad2817a489d8856dc9a9e6
Brian L. Chen <brian.lxchen@gmail.com>
2021-11-07 12:28:02 +0800

    Add more customizable options to Button

--- src/partials/basic/Button.js
+++ src/partials/basic/Button.js
@@ -1,3 +1,4 @@
+/* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
 
 const getClassNames = (variant, color) => {
@@ -14,6 +15,9 @@ const getClassNames = (variant, color) => {
     case 'secondary-danger': {
       return 'btn bg-white border-gray-200 hover:border-gray-300 active:border-gray-400 text-red-500';
     }
+    case 'secondary-tertiary': {
+      return 'btn bg-white border-gray-200 hover:border-gray-300 text-gray-600 active:border-gray-400';
+    }
     default: {
       return 'btn border-gray-200 hover:border-gray-300 active:border-gray-400 text-gray-600';
     }
@@ -29,13 +33,22 @@ export default function Button({
   disabled,
   onClick,
   className,
+  notHidden,
+  ...rest
 }) {
   return (
-    <button className={`${getClassNames(variant, color)} disabled:border-gray-200 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed shadow-none ${className}`} type={type} disabled={disabled} onClick={onClick}>
-      {icon}
-      <span className={`${icon && children && 'hidden xs:block ml-2'}`}>
-        {children}
-      </span>
+    <button
+      className={`${getClassNames(
+        variant,
+        color,
+      )} disabled:border-gray-200 disabled:bg-gray-100 disabled:text-gray-400 disabled:cursor-not-allowed shadow-none ${className}`}
+      type={type}
+      disabled={disabled}
+      onClick={onClick}
+      {...rest}
+    >
+      <span className={`${notHidden && 'mr-2'}`}>{icon}</span>
+      <span className={`${icon && children && !notHidden && 'hidden xs:block ml-2'}`}>{children}</span>
     </button>
   );
 }
