217cd74dbe3f42f32c881db9694f7ae017154906
Brian L. Chen <brian.lxchen@gmail.com>
2021-11-07 12:24:57 +0800

    formatted by prettier

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import Icons from '../icons';
 import Button from '../partials/basic/Button';
-import DashboardCard from '../partials/DashboardCard';
 
 import '../css/additional-styles/horizontalScrollingMenu.css';
+import DashboardCard from '../partials/DashboardCard';
 import EventGallery from '../partials/EventGallery';
 
 function Dashboard() {
   return (
     <main>
       <div className="px-4 sm:px-6 lg:px-8 py-8 w-full max-w-9xl mx-auto">
-
         {/* Dashboard actions */}
         <div className="sm:flex sm:justify-between sm:items-center mb-8">
-
           <h1 className="text-2xl md:text-3xl text-gray-800 font-bold mb-1">Dashboard 🚗</h1>
           {/* Right: Actions */}
           <div className="grid grid-flow-col sm:auto-cols-max justify-start sm:justify-end gap-2">
-            <Button variant="secondary" icon={<Icons.Add />}>Add Event</Button>
+            <Button variant="secondary" icon={<Icons.Add />}>
+              Add Event
+            </Button>
           </div>
-
         </div>
 
         {/* Cards */}
@@ -29,7 +28,6 @@ function Dashboard() {
           </DashboardCard>
           <DashboardCard title="Your Upcoming Event ➡️️">
             <EventGallery eventIds={[1, 2, 3, 4]} />
-
           </DashboardCard>
           <DashboardCard title="Bookmarked Events 📌">
             <EventGallery eventIds={[1, 2, 3, 4]} />
@@ -42,9 +40,7 @@ function Dashboard() {
           <DashboardCard title="Events You Host 📣">
             <EventGallery eventIds={[1, 2, 3, 4]} />
           </DashboardCard>
-
         </div>
-
       </div>
     </main>
   );
