418d5184d66b8c0eafc3b00ddf80cf06d85bcfad
Brian L. Chen <brian.lxchen@gmail.com>
2021-11-07 12:25:41 +0800

    change friend page src

--- src/pages/Index.js
+++ src/pages/Index.js
@@ -9,7 +9,7 @@ import PageNotFound from './utility/PageNotFound';
 import Sidebar from '../partials/Sidebar';
 import Header from '../partials/Header';
 import History from './History';
-import Friends from '../partials/Friends';
+import Friends from './Friends';
 
 export default function Index() {
   const [sidebarOpen, setSidebarOpen] = useState(false);
