8511a971c1776ff0da3f651d42f26515ed915673
Brian L. Chen <brian.lxchen@gmail.com>
2021-11-07 20:31:06 +0800

    use button components

--- src/partials/basic/UserItem.js
+++ src/partials/basic/UserItem.js
@@ -1,5 +1,8 @@
 import React from 'react';
 import Avatar from './Avatar';
+import Button from './Button';
+import Trashcan from '../../icons/Trashcan';
+import Check from '../../icons/Check';
 
 export default function UserItem({ setFriendSidebarOpen, userName, isActive, request }) {
   return (
@@ -20,16 +23,8 @@ export default function UserItem({ setFriendSidebarOpen, userName, isActive, req
               </div>
             </div>
             <div className="hidden group-hover:flex flex items-center">
-              <button className="btn-sm border-gray-200 mx-1 hover:border-gray-300">
-                <svg className="w-4 h-4 fill-current text-red-500 flex-shrink-0" viewBox="0 0 16 16">
-                  <path d="M5 7h2v6H5V7zm4 0h2v6H9V7zm3-6v2h4v2h-1v10c0 .6-.4 1-1 1H2c-.6 0-1-.4-1-1V5H0V3h4V1c0-.6.4-1 1-1h6c.6 0 1 .4 1 1zM6 2v1h4V2H6zm7 3H3v9h10V5z" />
-                </svg>
-              </button>
-              <button className="btn-sm border-gray-200 hover:border-gray-300">
-                <svg className="w-4 h-4 fill-current text-indigo-500 flex-shrink-0" viewBox="0 0 16 16">
-                  <path d="M14.3 2.3L5 11.6 1.7 8.3c-.4-.4-1-.4-1.4 0-.4.4-.4 1 0 1.4l4 4c.2.2.4.3.7.3.3 0 .5-.1.7-.3l10-10c.4-.4.4-1 0-1.4-.4-.4-1-.4-1.4 0z" />
-                </svg>
-              </button>
+              <Button icon={<Trashcan />} type="button" variant="secondary" color="danger" className="px-1.5 py-1.5 ml-1 my-0" />
+              <Button icon={<Check />} type="button" variant="secondary" color="default" className="px-1.5 py-1.5 ml-1 my-0" />
             </div>
           </div>
         </button>
